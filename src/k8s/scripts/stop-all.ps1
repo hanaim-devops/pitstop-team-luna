3c22a5b68aeb8b9f46ab5c20df5a0bcdf3d1cc97
Tim van de Ven <tim.vandeven@afas.nl>
2024-11-01 09:26:58 +0100

    Fix stop all script

--- src/k8s/scripts/stop-all.ps1
+++ src/k8s/scripts/stop-all.ps1
@@ -1,5 +1,9 @@
 kubectl delete svc --all -n pitstop
 kubectl delete deploy --all -n pitstop
+kubectl delete virtualservice --all -n pitstop
+kubectl delete destinationrule --all -n pitstop
 
 kubectl delete svc --all -n monitoring
 kubectl delete deploy --all -n monitoring
+kubectl delete virtualservice --all -n monitoring
+kubectl delete destinationrule --all -n monitoring
--- src/k8s/scripts/stop-all.sh
+++ src/k8s/scripts/stop-all.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 kubectl delete svc --all -n pitstop
 kubectl delete deploy --all -n pitstop
+kubectl delete virtualservice --all -n pitstop
+kubectl delete destinationrule --all -n pitstop
 
 kubectl delete svc --all -n monitoring
 kubectl delete deploy --all -n monitoring
+kubectl delete virtualservice --all -n monitoring
+kubectl delete destinationrule --all -n monitoring
