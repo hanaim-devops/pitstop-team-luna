f6ec5ca32d589b8129726413ae2cc6c8db1f8fc7
Tim van de Ven <tim.vandeven@afas.nl>
2024-11-01 09:36:27 +0100

    !Verwijder configmaps in stop-all script

--- src/k8s/scripts/stop-all.ps1
+++ src/k8s/scripts/stop-all.ps1
@@ -1,9 +1,6 @@
 kubectl delete svc --all -n pitstop
 kubectl delete deploy --all -n pitstop
-kubectl delete virtualservice --all -n pitstop
-kubectl delete destinationrule --all -n pitstop
 
 kubectl delete svc --all -n monitoring
 kubectl delete deploy --all -n monitoring
-kubectl delete virtualservice --all -n monitoring
-kubectl delete destinationrule --all -n monitoring
+kubectl delete configmap --all -n monitoring
